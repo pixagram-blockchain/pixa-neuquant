b25404e7c4e93fd90f1be4abdf3f28efb9994494
Matias Affolter <matiasaffolter@gmail.com>
2025-04-24 10:17:38 +0200

    modified:   Cargo.toml 	modified:   target/wasm32-unknown-unknown/release/neuquant_skin.d

--- target/wasm32-unknown-unknown/release/neuquant_skin.d
+++ target/wasm32-unknown-unknown/release/neuquant_skin.d
@@ -1,1 +1,1 @@
-/home/asaitama/Documents/neuquant_skin/target/wasm32-unknown-unknown/release/neuquant_skin.wasm: /home/asaitama/Documents/neuquant_skin/src/lib.rs /home/asaitama/Documents/neuquant_skin/src/math.rs
+/home/asaitama/Documents/neuquant_skin/pixa-neuquant/target/wasm32-unknown-unknown/release/neuquant_skin.wasm: /home/asaitama/Documents/neuquant_skin/pixa-neuquant/src/lib.rs /home/asaitama/Documents/neuquant_skin/pixa-neuquant/src/math.rs
